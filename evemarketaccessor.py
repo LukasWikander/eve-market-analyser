42b9a54d5610b929dec8d3500b427440e6783f69
Lukas Wikander <lukas.wikander@gmail.com>
2020-11-30 22:09:39 +0100

    Added latest changes

--- evemarketaccessor.py
+++ evemarketaccessor.py
@@ -12,10 +12,43 @@ import requests
 import json
 import yaml
 
-class MarketAccessor:
+class TypeIDMap:
     def __init__(self):
+        df = pd.read_csv("typeids.csv", names=['index','name','type_id'])
+        df.set_index('index',inplace=True)
+        self.map = df.drop('type_id',axis=1).transpose().to_dict('index')['name']
+        self.reverse_map = {y:x for x,y in self.map.items()}
+
+    def convert(self,data):
+        try:
+            test_val = data.iloc[0]
+        except AttributeError:
+            data = pd.Series(data)
+            test_val = data.iloc[0]
+        if type(test_val) is str:
+            return data.map(self.reverse_map)
+        else:
+            return data.map(self.map)
+
+    def get_type_id_of(self,item_name):
+        for id, name_in_map in self.map.items():
+            if name_in_map == item_name:
+                return id
+        raise ValueError('Type ID for item "' + str(item_name) + '" not found')
+
+    def get_item_name_of(self,type_id):
+        return self.map[type_id]
+
+class MarketAccessor:
+    default_market_data_columns = ['item','price','region','range','volume_remain','volume_total','min_volume','type_id','order_id','location_id','system_id','is_buy_order','issued','duration']
+    def __init__(self, type_id_map=None):
         self.type_id_data = pd.read_csv('typeids.csv',names=['index','name','type_id'])
+        if type_id_map:
+            self.type_id_map = type_id_map
+        else:
+            self.type_id_map = TypeIDMap()
         self.region_id_data = self.fetch_region_id_data()
+        self.market_data = {}
 
     def fetch_region_id_data(self):
         df = pd.DataFrame(columns=['name','region_id'])
@@ -33,20 +66,11 @@ class MarketAccessor:
         df.rename(columns={'id': 'region_id'}, inplace=True)
         return df
 
-
     def get_type_id_of(self,item_name):
-        for _, row in self.type_id_data.iterrows():
-            if row['name'] == item_name:
-                return row['type_id']
-        raise ValueError('Type ID for item "' + str(item_name) + '" not found')
-
+        return self.type_id_map.get_type_id_of(item_name)
 
     def get_item_name_of(self,type_id):
-        for _, row in self.type_id_data.iterrows():
-            if row['type_id'] == type_id:
-                return row['name']
-        raise ValueError('Name for item ID "' + str(type_id) + '" not found')
-
+        return self.type_id_map.get_item_name_of(type_id)
 
     def get_region_id_of(self,region_name):
         for _, row in self.region_id_data.iterrows():
@@ -56,25 +80,38 @@ class MarketAccessor:
         
 
     def get_market_data(self,items=[],region_names=["The Forge"]):
-        market_data = pd.DataFrame()
-        for item in items:
-            for region_name in region_names:
-                data = self.send_market_data_request(item, region_name)
-                if market_data.empty:
-                    market_data = data
-                else:
-                    market_data = market_data.append(data)
-        return market_data
-
-    def send_market_data_request(self,item,region_name):
-        df = pd.DataFrame(columns=['item','price','region','range','volume_remain','volume_total','min_volume','type_id','order_id','location_id','system_id','is_buy_order','issued','duration'])
-        print("Getting market data from region " + str(region_name) + " for item " + str(item))
-        if type(item) is str:
-            type_id = self.get_type_id_of(item)
-            type_name = item
+        if items and type(items[0]) is str:
+            item_ids = self.type_id_map.convert(items)
         else:
-            type_id = item
-            type_name = self.get_item_name_of(item)
+            item_ids = items
+
+        type_id_filter = self.market_data[self.market_data['type_id'].isin(item_ids)]
+        return type_id_filter[type_id_filter['region'].isin(region_names)]
+
+    def update_market_data(self,region_names=["The Forge"]):
+        self.market_data = pd.DataFrame(columns=self.default_market_data_columns)
+        for region_name in region_names:
+            market_data = self.send_market_data_request(region_name=region_name)
+            market_data['region'] = region_name
+            self.market_data = self.market_data.append(market_data)
+        return self.market_data
+
+    def send_market_data_request(self,item=None,region_name=None):
+        df = pd.DataFrame(columns=self.default_market_data_columns)
+        if not region_name:
+            return df
+        if item:
+            print("Getting market data from region " + str(region_name) + " for item " + str(item))
+        else:
+            print("Getting market data from region " + str(region_name))
+        
+        if item:
+            if type(item) is str:
+                type_id = self.type_id_map.get_type_id_of(item)
+                type_name = item
+            else:
+                type_id = item
+                type_name = self.type_id_map.get_item_name_of(item)
             
         region_id = self.get_region_id_of(region_name)
         page_no = 1
@@ -82,23 +119,33 @@ class MarketAccessor:
             url = "https://esi.evetech.net/latest/markets/"
             url = url + str(region_id)
             url = url + "/orders/?datasource=tranquility&order_type=sell&page="
-            url = url + str(page_no) + "&type_id=" + str(type_id)
+            url = url + str(page_no)
+            if item:
+                url = url + "&type_id=" + str(type_id)
             resp = requests.get(url)
-            if not resp.ok:
-                raise requests.RequestException("HTTP request returned status code " + str(resp.status_code) + " for URL " + url)
-            json_response = json.loads(resp.content)
-            if not json_response:
-                break
-            else:
-                if df.empty:
-                    df = pd.DataFrame(json_response)
+            if resp.ok:
+                json_response = json.loads(resp.content)
+                if not json_response:
+                    break
                 else:
-                    df = df.append(json_response)
-            page_no = page_no + 1
-        
-        df['region'] = region_name
-        df['item'] = type_name
-        df = df[['item','price','region','range','volume_remain','volume_total','min_volume','type_id','order_id','location_id','system_id','is_buy_order','issued','duration']]
+                    if df.empty:
+                        df = pd.DataFrame(json_response)
+                    else:
+                        df = df.append(json_response)
+                page_no = page_no + 1
+            
+        try:
+            df['region'] = [region_name]
+        except ValueError:
+            df['region'] = region_name
+        if item:
+            try:
+                df['item'] = [type_name]
+            except ValueError:
+                df['item'] = type_name
+        else:
+            df['item'] = self.type_id_map.convert(df['type_id'])
+        df = df[self.default_market_data_columns]
         return df
 
 class BlueprintDatabase:
@@ -147,11 +194,13 @@ class BlueprintDatabase:
 
 if __name__ == "__main__":
     accessor = MarketAccessor()
-    market_data_frame = accessor.get_market_data(items=["Tritanium","Pyerite"],region_names=["The Forge"])
+    all_market_data = accessor.update_market_data(region_names=["Lonetrek","Black Rise"])
+    market_data_frame = accessor.get_market_data(items=["Tritanium","Pyerite"],region_names=["Lonetrek"])
+    #market_data_frame = market_data_frame[[]]
     print(market_data_frame)
-    bp_db = BlueprintDatabase(market_accessor=accessor)
-    bp_db.load('sde/fsd/blueprints.yaml')
-    market_data_frame = accessor.get_market_data(bp_db.data.iloc[0:3,9])
+    #bp_db = BlueprintDatabase(market_accessor=accessor)
+    #bp_db.load('sde/fsd/blueprints.yaml')
+    #market_data_frame = accessor.get_market_data(bp_db.data.iloc[0:3,9])
 
-    print(market_data_frame)
+    #print(market_data_frame)
 
--- production-cost-optimiser.py
+++ production-cost-optimiser.py
@@ -1,17 +1,64 @@
 # -*- coding: utf-8 -*-
 
 from evemarketaccessor import MarketAccessor, BlueprintDatabase
-#markets_of_intere
+import pandas as pd
+import time
 
+markets_of_interest=["The Forge","Black Rise","The Citadel","Lonetrek"]
+#markets_of_interest=["Black Rise","Lonetrek"]
+accessor = MarketAccessor()
+accessor.update_market_data(region_names=markets_of_interest)
+blueprints = BlueprintDatabase(market_accessor=accessor)
+blueprints.load('sde/fsd/blueprints.yaml')
 
+def get_trading_prices_by_region(type_ids):
+    trading_prices_all = pd.DataFrame()
+    for type_id in type_ids:
+        trading_prices_all_regions = pd.DataFrame()
+        for market in markets_of_interest:
+            market_data_frame = accessor.get_market_data(items=[type_id],region_names=[market])
+            try:
+                market_data_frame = market_data_frame.sort_values(by=['price'])
+                trading_prices_all_regions = trading_prices_all_regions.append(market_data_frame.iloc[0,:])
+            except:
+                print("Unable to get market data for " + str(type_id) + " from market " + str(market))
+        try:
+            if not trading_prices_all_regions['price'].isnull().all():
+                trading_prices_all_regions = trading_prices_all_regions.sort_values(by=['price'],ascending=False)
+                trading_prices_all = trading_prices_all.append(trading_prices_all_regions)
+        except KeyError:
+            print("No market data for item ID " + str(type_id))
+    return trading_prices_all
+
+def find_best_markup(trading_prices, type_ids):
+    markup_data = pd.DataFrame()
+    for type_id in type_ids:
+        type_trading_prices = trading_prices[trading_prices['type_id'] == type_id]
+        if type_trading_prices.empty or is_unique(type_trading_prices['region']):
+            continue
+        type_trading_prices = type_trading_prices.sort_values(by=['price'])
+        data_point = {'type_id': type_id, 'buy_price': type_trading_prices['price'].iloc[0],'sell_price': type_trading_prices['price'].iloc[-1:],
+                        'buy_location': type_trading_prices['location_id'].iloc[0],'sell_location': type_trading_prices['location_id'].iloc[-1:],
+                        'buy_region': type_trading_prices['region'].iloc[0],'sell_region': type_trading_prices['region'].iloc[-1:]}
+        data_point['markup'] = data_point['sell_price'] / data_point['buy_price']
+        markup_data = markup_data.append(pd.DataFrame(data_point))
+    markup_data['item'] = accessor.type_id_map.convert(markup_data['type_id'])
+    return markup_data.sort_values(by=['markup'])
+
+def is_unique(s):
+    a = s.to_numpy()
+    return (a[0] == a).all()
 
 if __name__ == "__main__":
-    accessor = MarketAccessor()
-    market_data_frame = accessor.get_market_data(items=["Tritanium","Pyerite"],region_names=["The Forge"])
-    print(market_data_frame)
-    bp_db = BlueprintDatabase(market_accessor=accessor)
-    bp_db.load('sde/fsd/blueprints.yaml')
-    print(bp_db.data)
-    market_data_frame = accessor.get_market_data(bp_db.data.iloc[0:3,9])
+    trading_prices = get_trading_prices_by_region(blueprints.data['type_id'])
+    markup_data = find_best_markup(trading_prices, blueprints.data['type_id'])
+    pd.set_option('display.max_rows',None)
+    markup_data = markup_data[['item','markup','buy_price','sell_price','buy_location','sell_location','buy_region','sell_region','type_id']]
+    print(markup_data)
 
-    print(market_data_frame)
+    #market_data_frame = accessor.get_market_data(items=["Tritanium","Pyerite"],region_names=markets_of_interest)
+    #print(market_data_frame)
+    #print(bp_db.data)
+    #market_data_frame = accessor.get_market_data(bp_db.data.iloc[0:3,9])
+#
+    #print(market_data_frame)
